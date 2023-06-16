68965d6b4ea87134e1e4ff158e802f06225c86d7
andi <andreasweber2001@outlook.de>
2023-06-16 18:38:37 +0200

    seaborn plot

--- program.py
+++ program.py
@@ -1,13 +1,19 @@
 import numpy as np
-import tensorflow as tf
+# import tensorflow as tf
 import seaborn as sns
+from matplotlib import pyplot as plt
+
+sns.set()
 
 x = np.arange(1, 5, 0.01)
-noi = np.random.normal(0, 0.3, len(x))
+noi = np.random.normal(0, 0.5, len(x))
 y = 2 * x + noi
 
-sns.set_theme()
 
-tips = sns.load_dataset("tips")
+fig, ax = plt.subplots(figsize = (16, 8))
 
-print(tips.datatype)
+ax.scatter(x, y, color = "royalblue")
+ax.set_xlabel("time")
+ax.set_ylabel("position")
+
+plt.show()
