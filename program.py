f2612f8d6d685f38467583347d5eab4bf4049c2e
andi <andreasweber2001@outlook.de>
2023-06-16 18:21:37 +0200

    changes with a test plot

--- program.py
+++ program.py
@@ -1,3 +1,13 @@
 import numpy as np
 import tensorflow as tf
+import seaborn as sns
 
+x = np.arange(1, 5, 0.01)
+noi = np.random.normal(0, 0.3, len(x))
+y = 2 * x + noi
+
+sns.set_theme()
+
+tips = sns.load_dataset("tips")
+
+print(tips.datatype)
