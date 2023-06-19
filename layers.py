acfd2cede3beb73e2d445b1724329cf1804def18
andi <andreasweber2001@outlook.de>
2023-06-19 22:50:40 +0200

    cool history plot

--- layers.py
+++ layers.py
@@ -2,34 +2,60 @@ import numpy as np
 import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense
+import seaborn as sns
 
 # Generate quadratic data with noise
 np.random.seed(42)
-X = np.random.uniform(low=-1, high=1, size=(100, 1))
-y = 2 * X**2 + np.random.normal(scale=0.1, size=(100, 1))
+X = np.random.uniform(low=-10, high=10, size=(200, 1))
+y = 2 * X ** 2 + np.random.normal(scale=0.1, size=(200, 1))
 
 # Create the model
 model = Sequential()
-model.add(Dense(10, input_shape=(1,), activation='relu'))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(10, activation='relu'))
+model.add(Dense(15, input_shape=(1,), activation='relu'))
+model.add(Dense(25, activation='relu'))
+model.add(Dense(35, activation='relu'))
+model.add(Dense(45, activation='relu'))
+model.add(Dense(55, activation='relu'))
+model.add(Dense(65, activation='relu'))
+model.add(Dense(55, activation='relu'))
+model.add(Dense(45, activation='relu'))
+model.add(Dense(35, activation='relu'))
+model.add(Dense(25, activation='relu'))
+model.add(Dense(15, activation='relu'))
 model.add(Dense(1))
+
+model.summary()
 
 # Compile and train the model
 model.compile(optimizer='adam', loss='mse')
-model.fit(X, y, epochs=100, verbose=0)
+history = model.fit(X, y, epochs=200, verbose=0)
+
+
+# Extract the MSE values and epoch numbers from the training history
+mse = history.history['loss']
+epochs = range(1, len(mse) + 1)
+
+# Plot the MSE versus epoch
+plt.plot(epochs, mse, 'b-o')
+plt.xlabel('Epoch')
+plt.ylabel('Mean Squared Error')
+plt.title('Mean Squared Error vs. Epoch')
+plt.grid(True)
+plt.show()
+
+
 
 # Generate test data
-X_test = np.linspace(-1, 1, 100).reshape(-1, 1)
+X_test = np.linspace(-10, 10, 200).reshape(-1, 1)
 
 # Predict using the trained model
 y_pred = model.predict(X_test)
 
-# Plot the original data and the predicted values
-plt.scatter(X, y, label='Original Data')
-plt.plot(X_test, y_pred, 'r-', label='Predicted Values')
+# Plot
+sns.set(style="whitegrid")
+plt.figure(figsize=(8, 6))
+sns.scatterplot(x=X.flatten(), y=y.flatten(), label='Original Data', color = "royalblue")
+sns.lineplot(x=X_test.flatten(), y=y_pred.flatten(), color='crimson', label='Predicted Values')
 plt.xlabel('X')
 plt.ylabel('y')
 plt.title('Regression on Quadratic Data')
